59cab60528d118a9a6ff82f83dcebe73b1aebf8c
chaurasiyajitendra <jitendragta902@gmail.com>
2024-07-04 19:03:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function Scard(){
+   document.getElementById("sclnv").innerHTML= document.getElementById("schoolName").value;
+   document.getElementById("stunv").innerHTML =document.getElementById("stulName").value; 
+   document.getElementById("stub").innerHTML =document.getElementById("bod").value; 
+   document.getElementById("stucn").innerHTML =document.getElementById("num").value; 
+   document.getElementById("stucc").innerHTML =document.getElementById("std").value; 
+
+}
+function pit()
+{
+    window.print()
+    alert("YOUR CARD PRINT !! THANK YOU ")
+}
+
